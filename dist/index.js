66837980defa49c67394d10a5b3e46eafdf65f6c
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-06-07 22:37:38 +0200

    Intent inherits from the EventEmitter class.

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@
 		var a = typeof exports === 'object' ? factory(require("promise"), require("events")) : factory(root["promise"], root["events"]);
 		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
 	}
-})(this, function(__WEBPACK_EXTERNAL_MODULE_2__, __WEBPACK_EXTERNAL_MODULE_4__) {
+})(this, function(__WEBPACK_EXTERNAL_MODULE_2__, __WEBPACK_EXTERNAL_MODULE_3__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -66,7 +66,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _libIntent2 = _interopRequireDefault(_libIntent);
 
-	var _libIntents = __webpack_require__(3);
+	var _libIntents = __webpack_require__(4);
 
 	var _libIntents2 = _interopRequireDefault(_libIntents);
 
@@ -87,18 +87,25 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
 
 	var _promise = __webpack_require__(2);
 
 	var _promise2 = _interopRequireDefault(_promise);
 
-	var Intent = (function () {
+	var _events = __webpack_require__(3);
+
+	var Intent = (function (_EventEmitter) {
 	    function Intent(params, key) {
 	        _classCallCheck(this, Intent);
 
+	        _get(Object.getPrototypeOf(Intent.prototype), 'constructor', this).call(this);
 	        this._params = params;
 	        if (key) {
 	            this.key = key;
@@ -109,6 +116,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	        }).bind(this));
 	        this._handled = false;
 	    }
+
+	    _inherits(Intent, _EventEmitter);
 
 	    _createClass(Intent, [{
 	        key: 'then',
@@ -147,7 +156,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    }]);
 
 	    return Intent;
-	})();
+	})(_events.EventEmitter);
 
 	exports['default'] = Intent;
 	module.exports = exports['default'];
@@ -162,6 +171,12 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
+	module.exports = __WEBPACK_EXTERNAL_MODULE_3__;
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
 	'use strict';
 
 	Object.defineProperty(exports, '__esModule', {
@@ -170,7 +185,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-	var _events = __webpack_require__(4);
+	var _events = __webpack_require__(3);
 
 	var _Intent = __webpack_require__(1);
 
@@ -237,12 +252,6 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	exports['default'] = Intents;
 	module.exports = exports['default'];
-
-/***/ },
-/* 4 */
-/***/ function(module, exports, __webpack_require__) {
-
-	module.exports = __WEBPACK_EXTERNAL_MODULE_4__;
 
 /***/ }
 /******/ ])
